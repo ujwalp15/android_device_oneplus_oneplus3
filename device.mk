0a26afabafe8c8d867db8e1a6699833a30d6820e
Luca Stefani <luca020400@lineageos.org>
2017-08-25 18:55:56 +0530

    op3: bluetooth: Add libbt-vendor in device.mk

--- device.mk
+++ device.mk
@@ -153,8 +153,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbt-vendor \
     android.hardware.bluetooth@1.0-impl
-	
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/iot_devlist.conf:system/etc/bluetooth/iot_devlist.conf
 
