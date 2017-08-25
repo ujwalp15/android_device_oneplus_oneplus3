a49c3fc9c5b8dd9493419590d48371384c861aa6
Luca Stefani <luca020400@lineageos.org>
2017-08-25 19:13:13 +0530

    op3: Update audio for O
    * Build audio HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL

--- device.mk
+++ device.mk
@@ -112,17 +112,23 @@ PRODUCT_BOOT_JARS += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
-    audio.a2dp.default \
     audio.primary.msm8996 \
-    audio.r_submix.default \
+    audio.a2dp.default \
     audio.usb.default \
-    libaudio-resampler \
-    libqcompostprocbundle \
-    libqcomvisualizer \
+	audio.r_submix.default \
+    libaudio-resampler
+
+PRODUCT_PACKAGES +=
+	libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+	libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
