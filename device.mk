7b716de357bc5367e10b351141172bbf93f9185f
Luca Stefani <luca020400@lineageos.org>
2017-08-25 19:23:35 +0530

    op3: Build wifilogd
    * Remove unused libqsap_sdk & libQWiFiSoftApCfg

--- device.mk
+++ device.mk
@@ -364,10 +364,9 @@ PRODUCT_PACKAGES += \
     ipacm \
     ipacm-diag \
     IPACM_cfg.xml \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     wificond \
+	wifilogd \
     hostapd \
     dhcpcd.conf \
     wpa_supplicant \
