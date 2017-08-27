847b6d51b9a7e4dbcb8f7cbb68fd3fb25e1d2094
ujwalp15 <ujwalp09@gmail.com>
2017-08-27 08:08:45 +0530

    op3: Switch to HIDL multihal

--- device.mk
+++ device.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    sensors.msm8996
+    android.hardware.sensors@1.0-impl
 
 # Thermal
 PRODUCT_PACKAGES += \
