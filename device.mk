d4ffdbdcb781beb3b5657afa5d4a1c7fb1b05d49
ujwalp15 <ujwalp09@gmail.com>
2017-08-28 06:30:49 +0530

    op3: disable additional boot jars

--- device.mk
+++ device.mk
@@ -108,8 +108,8 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+# PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # Audio
 PRODUCT_PACKAGES += \
